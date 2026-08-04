539c8d8cf58c01965176cce287b260108a872a40
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-08-04 00:42:05 -0300

    feat: filtra avaliações por categoria musical

--- Aula 1.1 - Introdução à programação funcional/oficina.py
+++ Aula 1.1 - Introdução à programação funcional/oficina.py
@@ -8,6 +8,7 @@ notas_pop = [
     4, 2, 1, 2, 5, 2, 4, 4, 0, 1
 ]
 
+
 def categorizar(nota):
     if 0 <= nota <= 1:
         return "ruim"
@@ -26,4 +27,38 @@ print(categorias_rock)
 print("\nCategorias de Pop:")
 print(categorias_pop)
 
-input ("\nPressione Enter para encerrar...")
+rock_ruins = list(
+    filter(lambda categoria: categoria == "ruim", categorias_rock)
+)
+
+rock_medianas = list(
+    filter(lambda categoria: categoria == "mediana", categorias_rock)
+)
+
+rock_boas = list(
+    filter(lambda categoria: categoria == "boa", categorias_rock)
+)
+
+pop_ruins = list(
+    filter(lambda categoria: categoria == "ruim", categorias_pop)
+)
+
+pop_medianas = list(
+    filter(lambda categoria: categoria == "mediana", categorias_pop)
+)
+
+pop_boas = list(
+    filter(lambda categoria: categoria == "boa", categorias_pop)
+)
+
+print("\nRock:")
+print("Ruins:", rock_ruins)
+print("Medianas:", rock_medianas)
+print("Boas:", rock_boas)
+
+print("\nPop:")
+print("Ruins:", pop_ruins)
+print("Medianas:", pop_medianas)
+print("Boas:", pop_boas)
+
+input("\nPressione Enter para encerrar...")
