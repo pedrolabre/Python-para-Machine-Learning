ad6b30a372bcf4f0567db10489a3ae2238fa3354
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-08-05 09:40:43 -0300

    feat: adiciona datas de aniversário e formatos correspondentes

---
+++ Aula 1.3 - Trabalhando com datas/oficina.py
@@ -0,0 +1,22 @@
+aniversarios = [
+    "01/02/1990",
+    "22 de Maio de 1991",
+    "04/Abr/1995",
+    "1995-Outubro-10",
+    "12 Julho 1989",
+    "16 de Junho de 1987",
+    "04/07/1990"
+]
+
+formatos = [
+    "%d/%m/%Y",
+    "%d de %B de %Y",
+    "%d/%b/%Y",
+    "%Y-%B-%d",
+    "%d %B %Y",
+    "%d de %B de %Y",
+    "%d/%m/%Y"
+]
+
+
+input("\nPressione Enter para encerrar...")
