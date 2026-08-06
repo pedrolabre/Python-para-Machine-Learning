77337e2ee5535f52a61fc8c2b5fb666d0576b28c
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-08-06 16:08:39 -0300

    feat: adiciona menu e repetição das rodadas

---
+++ Aula 2 - Introdução a bibliotecas matemáticas/oficina.py
@@ -0,0 +1,14 @@
+while True:
+    print("\nEscolha uma opção (1, 2, 3 ou 4):")
+    print("1-Pedra")
+    print("2-Papel")
+    print("3-Tesoura")
+    print("4-Sair")
+
+    escolha_jogador = input()
+
+    if escolha_jogador == "4":
+        print("\nJogo encerrado.")
+        break
+
+input("\nPressione Enter para encerrar...")
