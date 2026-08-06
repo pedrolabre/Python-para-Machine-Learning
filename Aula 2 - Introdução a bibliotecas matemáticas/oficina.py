f1dada06e0916beee7637399e0b6a6b9e462b26c
Pedro Roberto Ribeiro Bandeira Labre <pedrobandeiralabre@gmail.com>
2026-08-06 16:32:07 -0300

    feat: valida e identifica a jogada do jogador

--- Aula 2 - Introdução a bibliotecas matemáticas/oficina.py
+++ Aula 2 - Introdução a bibliotecas matemáticas/oficina.py
@@ -10,5 +10,16 @@ while True:
     if escolha_jogador == "4":
         print("\nJogo encerrado.")
         break
+    elif escolha_jogador == "1" or escolha_jogador == "2" or escolha_jogador == "3":
+        if escolha_jogador == "1":
+            jogada_jogador = "Pedra"
+        elif escolha_jogador == "2":
+            jogada_jogador = "Papel"
+        else:
+            jogada_jogador = "Tesoura"
+
+        print(f"\nSua jogada -> {jogada_jogador}")
+    else:
+        print("\nOpção inválida.")
 
 input("\nPressione Enter para encerrar...")
